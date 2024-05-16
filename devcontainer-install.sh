f6baf0354ebc6dd78c137cd210f222947534eb1b
Nate Hardison <natehardison@gmail.com>
2024-05-16 15:03:20 -0700

    Install git-delta in devcontainers

--- devcontainer-install.sh
+++ devcontainer-install.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
 make bash bat bin zsh
+
+# install git-delta
+sudo chown $USER $HOME/.local
+export TARGET=$HOME/.local/bin/
+mkdir $TARGET
+curl --silent --location \
+    https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
+    sh
+ubi -p dandavison/delta -i $TARGET
