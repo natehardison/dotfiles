46140d9fbc8892f0be87e0bb20d97b701428a986
Nate Hardison <natehardison@gmail.com>
2024-05-17 12:22:12 -0700

    Install common utilities in devcontainers

--- devcontainer-install.sh
+++ devcontainer-install.sh
@@ -2,13 +2,22 @@
 
 set -e
 
-make bash bat bin zsh
-
-# install git-delta
+# install ubi for installing from GitHub
 sudo chown $USER $HOME/.local
 export TARGET=$HOME/.local/bin/
 mkdir $TARGET
 curl --silent --location \
     https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
     sh
+
+# install utilities
+ubi -p sharkdp/bat -i $TARGET
 ubi -p dandavison/delta -i $TARGET
+ubi -p eza-community/eza -i $TARGET
+ubi -p sharkdp/fd -i $TARGET
+ubi -p junegunn/fzf -i $TARGET
+ubi -p jesseduffield/lazygit -i $TARGET
+ubi -p BurntSushi/ripgrep -i $TARGET
+ubi -p ajeetdsouza/zoxide -i $TARGET
+
+make bash bat bin zsh
